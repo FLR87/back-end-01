aad5cb7a751d6d152cd474bf542da133dba4a627
Alex <alex_shu17@mail.ru>
2017-11-24 15:09:32 +0200

    final push after comments

--- homework_2/index.php
+++ homework_2/index.php
@@ -2,8 +2,8 @@
 
 // Идея: по дефолту "myFunction" должна к каждому элементу массива прибавлять единицу и выводить получившийся результат;
 // после прибавления единицы, колбэком каждый элемент массива должен быть поделён на 2 и вывестись соответствующий результат.
-$arr1 = [15, 12, 7, 21, 4, 17];
-$arr2 = [8, 14, 2, 31, 11, 9];
+$arrFst = [15, 12, 7, 21, 4, 17];
+$arrNd = [8, 14, 2, 31, 11, 9];
 
 /** ОСНОВНАЯ ФУНКЦИЯ
  * @param $array //входящий массив
@@ -11,9 +11,9 @@ $arr2 = [8, 14, 2, 31, 11, 9];
  * @return mixed
  */
 function myFunction($array, $callback) {
-    for ($i = 0; $i < count($array); $i++) {
-        $array[$i] = $array[$i] + 1;
-    }
+    foreach ($array as &$i) {
+       $i = $i + 1;
+    };
 
     if(is_callable($callback)) {
         $array = call_user_func($callback, $array);
@@ -26,16 +26,16 @@ function myFunction($array, $callback) {
  * @return mixed
  */
 function callbackFunction ($array) {
-    for ($i = 0; $i < count($array); $i++) {
-        $array[$i] = $array[$i] / 2;
-    }
+    foreach ($array as &$i) {
+        $i = $i / 2;
+    };
     return $array;
 };
 
-$callback1 = "callbackFunction"; //ссылка на дополнительную функцию-обработчик
+$callbackFst = "callbackFunction"; //ссылка на дополнительную функцию-обработчик
 
-myFunction($arr1, $callback1); //первая обработка
+myFunction($arrFst, $callbackFst); //первая обработка
 echo "<br>";
-myFunction($arr2, $callback1); //вторая обработка
+myFunction($arrNd, $callbackFst); //вторая обработка
 
-?>
+?>;
