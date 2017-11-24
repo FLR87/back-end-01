cd61349f6da23c7014f2692f3cbdad4d568c3825
Alex <alex_shu17@mail.ru>
2017-11-24 14:16:53 +0200

    final push after comments

--- homework_1/index.php
+++ homework_1/index.php
@@ -5,17 +5,23 @@ $arrayCount = count($array);
 echo $arrayCount."<br>";
 echo ""."<br>";
 //2
-$positiveNumbers = function ($array) {
+/**
+ * @param $array
+ */
+function positiveNumbers($array) {
     for ($i = 0; $i < count($array); $i++) {
         if ($array[$i] % 5 == 0 && $array[$i] > 0) {
             echo $array[$i]."<br>";
         }
     }
 };
-echo $positiveNumbers($array);
+echo positiveNumbers($array);
 echo ""."<br>";
 //3
-$filesWithNumbers = function ($array) {
+/**
+ * @param $array
+ */
+function filesWithNumbers($array) {
     foreach($array as $number){
         switch($number) {
             case $number > 0:
@@ -30,20 +36,27 @@ $filesWithNumbers = function ($array) {
         }
     }
 };
-echo $filesWithNumbers($array);
+echo filesWithNumbers($array);
 
 // метод пузырьков
 
-for($i=0; $i<count($array); $i++){
-    for($j=$i+1; $j<count($array); $j++){
-        if($array[$i] > $array[$j]){
-            $temp = $array[$j];
-            $array[$j] = $array[$i];
-            $array[$i] = $temp;
+/**
+ * @param $array
+ */
+function bubble($array) {
+    for($i=0; $i<count($array); $i++){
+        for($j=$i+1; $j<count($array); $j++){
+            if($array[$i] > $array[$j]){
+                $temp = $array[$j];
+                $array[$j] = $array[$i];
+                $array[$i] = $temp;
+            }
         }
     }
-}
-print_r($array);
+    print_r($array);
+};
+echo bubble($array);
+
 ?>
 
 
